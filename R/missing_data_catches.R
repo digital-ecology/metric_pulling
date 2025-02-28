0d50cb1636d87272f49997ef0df5f3325b1b48d8
madisonellie <maddie@digital-ecology.co.uk>
2025-02-28 08:53:40 +0000

    moving errors to initialise out side of loop

--- R/missing_data_catches.R
+++ R/missing_data_catches.R
@@ -10,6 +10,8 @@ clean_c1_dataset <- function(metric) {
   
   #read the dataset
   df <- openxlsx::read.xlsx(metric, "C-1 On-Site WaterC' Baseline", cols = c(4,5,6,8,10,23,24,25,26), colNames = TRUE, startRow = 9)
+  
+  errormessages <- c()
   if(!is.na(df$Watercourse.type[1])) {
     
   colnames(df) <- gsub("\\.", " ", colnames(df))
@@ -19,7 +21,7 @@ clean_c1_dataset <- function(metric) {
   numberrows <- sum(!is.na(df[[1]])) #number of non NA vals
   df <- df[1:numberrows, ] #chop dataframe beyond numberrows
   df <- df[rowSums(is.na(df)) < ncol(df), ] #remove any straggling rows which are fully NA values
-  errormessages <- c()
+  
   
   #first thing that needs to be checked, is if there are any NAs in any column 
   for (i in 1:ncol(df)) { #for each column in the dataframe
@@ -47,6 +49,8 @@ clean_c2_dataset <- function(metric) {
   
   #read the dataset
   df <- openxlsx::read.xlsx(metric, "C-2 On-Site WaterC' Creation", cols = c(3,4,5,7,9,26,29), colNames = TRUE, startRow = 11)
+  
+  errormessages <- c()
   if(!is.na(df$Watercourse.type[1])) {
     
   colnames(df) <- gsub("\\.", " ", colnames(df))
@@ -57,7 +61,7 @@ clean_c2_dataset <- function(metric) {
   numberrows <- sum(!is.na(df[[2]])) #number of non NA vals
   df <- df[1:numberrows, ] #chop dataframe beyond numberrows
   df <- df[rowSums(is.na(df)) < ncol(df), ] #remove any straggling rows which are fully NA values
-  errormessages <- c()
+  
   
   #first thing that needs to be checked, is if there are any NAs in any column 
   for (i in 1:ncol(df)) { #for each column in the dataframe
@@ -86,6 +90,9 @@ clean_c3_dataset <- function(metric) {
   
   #read the dataset
   df <- openxlsx::read.xlsx(metric, "C-3 On-Site WaterC' Enhancement", cols = c(3,7,14,17,18,20,22,39), colNames = TRUE, startRow = 11)
+  
+  errormessages <- c()
+  
   if(!is.na(df$Baseline.habitat[1])) {
     
   colnames(df)[colnames(df) == "X3"] <- "Proposed Habitats"
@@ -97,7 +104,7 @@ clean_c3_dataset <- function(metric) {
   numberrows <- sum(!is.na(df[[2]])) #number of non NA vals
   df <- df[1:numberrows, ] #chop dataframe beyond numberrows
   df <- df[rowSums(is.na(df)) < ncol(df), ] #remove any straggling rows which are fully NA values
-  errormessages <- c()
+  
   
   #first thing that needs to be checked, is if there are any NAs in any column 
   for (i in 1:ncol(df)) { #for each column in the dataframe
@@ -322,6 +329,8 @@ clean_onsitehab_baseline <- function(metric) {
   
   #read the dataset
   df <- openxlsx::read.xlsx(metric, "A-1 On-Site Habitat Baseline", cols = c(4:6, 8:9, 11, 17:25), colNames = TRUE, startRow = 10)
+  
+  errormessages <- c()
   if(!is.na(df$Broad.Habitat[1])) {
     
   df$Area.retained[is.na(df$Area.retained)] <- 0
@@ -334,7 +343,7 @@ clean_onsitehab_baseline <- function(metric) {
   numberrows <- sum(!is.na(df[[2]])) #number of non NA vals
   df <- df[1:numberrows, ] #chop dataframe beyond numberrows
   df <- df[rowSums(is.na(df)) < ncol(df), ] #remove any straggling rows which are fully NA values
-  errormessages <- c()
+  
   
   #first thing that needs to be checked, is if there are any NAs in any column 
   for (i in 1:ncol(df)) { #for each column in the dataframe
@@ -363,6 +372,8 @@ clean_onsitehab_creation <- function(metric) {
   
   #read the dataset
   df <- openxlsx::read.xlsx(metric, "A-2 On-Site Habitat Creation", cols = c(4,5,7,8,10,12,19,25), colNames = TRUE, startRow = 10)
+  
+  errormessages <- c()
   if(!is.na(df$Condition[1])) {
     
   colnames(df)[colnames(df) == "X1"] <- "Broad Habitat"
@@ -376,7 +387,7 @@ clean_onsitehab_creation <- function(metric) {
   numberrows <- sum(!is.na(df[[1]])) #number of non NA vals
   df <- df[1:numberrows, ] #chop dataframe beyond numberrows
   df <- df[rowSums(is.na(df)) < ncol(df), ] #remove any straggling rows which are fully NA values
-  errormessages <- c()
+  
   
   #first thing that needs to be checked, is if there are any NAs in any column 
   for (i in 1:ncol(df)) { #for each column in the dataframe
@@ -406,6 +417,8 @@ clean_onsitehab_enhancement <- function(metric) {
   df <- openxlsx::read.xlsx(metric, sheet = "A-3 On-Site Habitat Enhancement", 
                             cols = c(6,17,18,22,23,25,27,30,40), 
                             colNames = TRUE, startRow = 11)
+  
+  errormessages <- c()
   if(!is.na(df$Baseline.habitat[1])) {
     
   colnames(df)[colnames(df) == "X5"] <- "Distinctiveness"
@@ -419,7 +432,7 @@ clean_onsitehab_enhancement <- function(metric) {
   numberrows <- sum(!is.na(df[[2]])) #number of non NA vals
   df <- df[1:numberrows, ] #chop dataframe beyond numberrows
   df <- df[rowSums(is.na(df)) < ncol(df), ] #remove any straggling rows which are fully NA values
-  errormessages <- c()
+  
   
   #first thing that needs to be checked, is if there are any NAs in any column 
   for (i in 1:ncol(df)) { #for each column in the dataframe
@@ -448,6 +461,8 @@ clean_onsitehedge_baseline <- function(metric) {
   
   #read the dataset
   df <- openxlsx::read.xlsx(metric, "B-1 On-Site Hedge Baseline", cols = c(3,4,5,8,10,14,16,17,18,19,20,21), colNames = TRUE, startRow = 9)
+  
+  errormessages <- c()
   if(!is.na(df$Hedge.number[1])) {
     
   df$Length.retained[is.na(df$Length.retained)] <- 0
@@ -459,7 +474,7 @@ clean_onsitehedge_baseline <- function(metric) {
   numberrows <- sum(!is.na(df[[2]])) #number of non NA vals
   df <- df[1:numberrows, ] #chop dataframe beyond numberrows
   df <- df[rowSums(is.na(df)) < ncol(df), ] #remove any straggling rows which are fully NA values
-  errormessages <- c()
+  
   
   #first thing that needs to be checked, is if there are any NAs in any column 
   for (i in 1:ncol(df)) { #for each column in the dataframe
@@ -489,7 +504,10 @@ clean_onsitehedge_creation <- function(metric) {
   
   #read the dataset
   df <- openxlsx::read.xlsx(metric, "B-2 On-Site Hedge Creation", cols = c(3,4,5,6,8,10,23), colNames = TRUE, startRow = 11)
-  if(!is.na(df$Habitat.type[1])) {
+   
+  errormessages <- c()
+  
+   if(!is.na(df$Habitat.type[1])) {
     
   colnames(df)[colnames(df) == "X7"] <- "Hedge Units Delivered"
   colnames(df) <- gsub("\\.", " ", colnames(df))
@@ -498,7 +516,7 @@ clean_onsitehedge_creation <- function(metric) {
   numberrows <- sum(!is.na(df[[2]])) #number of non NA vals
   df <- df[1:numberrows, ] #chop dataframe beyond numberrows
   df <- df[rowSums(is.na(df)) < ncol(df), ] #remove any straggling rows which are fully NA values
-  errormessages <- c()
+ 
   
   #first thing that needs to be checked, is if there are any NAs in any column 
   for (i in 1:ncol(df)) { #for each column in the dataframe
@@ -528,6 +546,8 @@ clean_onsitehedge_enhancement <- function(metric) {
   #read the dataset
   df <- openxlsx::read.xlsx(metric, "B-3 On-Site Hedge Enhancement", cols = c(2,3,7,16,17,19,21,34), colNames = TRUE, startRow = 11)
   
+  errormessages <- c()
+  
   if(!is.na(df$Baseline.habitat[1])) {
   colnames(df)[colnames(df) == "X4"] <- "Length (km)"
   colnames(df)[colnames(df) == "X8"] <- "Hedge Units Delivered"
@@ -537,7 +557,7 @@ clean_onsitehedge_enhancement <- function(metric) {
   numberrows <- sum(!is.na(df[[2]])) #number of non NA vals
   df <- df[1:numberrows, ] #chop dataframe beyond numberrows
   df <- df[rowSums(is.na(df)) < ncol(df), ] #remove any straggling rows which are fully NA values
-  errormessages <- c()
+  
   
   #first thing that needs to be checked, is if there are any NAs in any column 
   for (i in 1:ncol(df)) { #for each column in the dataframe
